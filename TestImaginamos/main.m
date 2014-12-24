0e3cf8b1430c6de64e2b6c71eec49fbe6b94df16
David Velarde <davidvpe@gmail.com>
2014-12-24 00:14:13 -0500

    Class Prefix fix part 2

--- TestImaginamos/main.m
+++ TestImaginamos/main.m
@@ -7,10 +7,10 @@
 //
 
 #import <UIKit/UIKit.h>
-#import "AppDelegate.h"
+#import "IMAAppDelegate.h"
 
 int main(int argc, char * argv[]) {
     @autoreleasepool {
-        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([IMAAppDelegate class]));
     }
 }
