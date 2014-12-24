58823cd2159a59b1970e9a01a70042c59e38f9ff
David Velarde <davidvpe@gmail.com>
2014-12-23 23:50:34 -0500

    Initial Commit

---
+++ TestImaginamos/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TestImaginamos
+//
+//  Created by David Velarde on 12/23/14.
+//  Copyright (c) 2014 Imaginamos. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ TestImaginamos/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  TestImaginamos
+//
+//  Created by David Velarde on 12/23/14.
+//  Copyright (c) 2014 Imaginamos. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
