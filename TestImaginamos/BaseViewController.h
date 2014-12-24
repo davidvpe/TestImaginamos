491d01aa8bae8e59ca11db20bc9a80001f2c4cc1
David Velarde <davidvpe@gmail.com>
2014-12-24 00:10:42 -0500

    Orientation Restriction Done

---
+++ TestImaginamos/BaseViewController.h
@@ -0,0 +1,13 @@
+//
+//  BaseViewController.h
+//  TestImaginamos
+//
+//  Created by David Velarde on 12/24/14.
+//  Copyright (c) 2014 Imaginamos. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BaseViewController : UIViewController
+
+@end
